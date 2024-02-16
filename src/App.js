919a8934fea84217121d31aee29ab1773be23e3f
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2024-02-16 17:59:15 +1100

    setting initial page and wrapping app.js with router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// styles
 import './App.css'
+
+// pages and components
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Project from './pages/project/Project'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/projects:id">
+              <Project />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
