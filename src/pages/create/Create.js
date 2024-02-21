1d590e616db4999806d5cb26988d72bf9ef418be
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2024-02-21 11:21:51 +1100

    add hooks for adding document to firebase database, preparing action in create.js to pass data to database

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react' 
 import Select from 'react-select'
 import { useCollection } from '../../hooks/useCollection'
+import { timestamp } from '../../firebase/config'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useHistory } from 'react-router-dom'
 
 // styles
 import './Create.css'
@@ -13,8 +17,11 @@ const categories = [
 ]
 
 export default function Create () {
+    const history = useHistory()
+    const {addDocument, response} = useFirestore('projects')
     const { documents } = useCollection('users')
     const [ users, setUsers ] = useState([])
+    const {user} = useAuthContext()
 
     // form field values
     const [name, setName] = useState('')
@@ -36,7 +43,7 @@ export default function Create () {
         }
     },[documents])
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         setFormError(null)
         if(!category) {
@@ -48,7 +55,34 @@ export default function Create () {
             return
         }
 
-        console.log(name,details,dueDate, category.value, assignedUsers)
+        const createdBy = {
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            id: user.uid
+        }
+
+        const assignedUsersList = assignedUsers.map((u) => {
+            return {
+                displayName: u.value.displayName,
+                photoURL: u.value.photoURL,
+                id: u.value.id
+            }
+        })
+
+        const project = {
+            name,
+            details,
+            category: category.value, 
+            dueDate: timestamp.fromDate(new Date(dueDate)),
+            comments:[],
+            createdBy,
+            assignedUsersList
+        }
+
+        await addDocument(project)
+        if(!response.error) {
+            history.push('/')
+        }
     }
     
     return (
