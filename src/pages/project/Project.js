a10dfb4f626d7521f0bc3cbb2f89652e5de842ce
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2024-02-23 22:51:49 +1100

    add ProjectSummary.js for showing project detail, add ProjectComments.js for comment input to the project

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useDocument } from '../../hooks/useDocument'
+import ProjectSummary from './ProjectSummary'
+import ProjectComments from './ProjectComments'
 
 // styles
 import './Project.css'
@@ -18,7 +20,8 @@ export default function Project () {
 
     return (
         <div className='project-details'>
-            <h1>{document.name}</h1>
+            <ProjectSummary project={document}/>
+            <ProjectComments />
         </div>
     )
 }
