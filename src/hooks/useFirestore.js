471bfb8f412cf91840438139e3db3231ae677751
Rafi <mcintoshhd@Mcintoshs-MacBook-Air.local>
2024-02-23 23:30:19 +1100

    create hook for add comment, integrate update document hook to ProjectComments.js, add UI logic to show comment, add css for comment

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -14,6 +14,8 @@ const firestoreReducer = (state, action) => {
       return { isPending: true, document: null, success: false, error: null }
     case 'ADDED_DOCUMENT':
       return { isPending: false, document: action.payload, success: true, error: null }
+    case 'UPDATED_DOCUMENT':
+      return { isPending: false, document: action.payload, success: true, error: null }
     case 'ERROR':
       return { isPending: false, document: null, success: false, error: action.payload }
     default:
@@ -49,10 +51,25 @@ export const useFirestore = (collection) => {
     }
   }
 
+  // update documents
+  const updateDocument = async (id, updates) => {
+    dispatch({ type: 'IS_PENDING' })
+
+    try {
+      const updatedDocument = await ref.doc(id).update(updates)
+      dispatchIfNotCancelled({ type: 'UPDATED_DOCUMENT', payload: updatedDocument })
+      return updatedDocument
+    } catch (err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
+      return null
+    }
+  }
+
+
   useEffect(() => {
     return () => setIsCancelled(true)
   }, [])
 
-  return { addDocument, response }
+  return { addDocument, updateDocument, response }
 
 }
--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,10 +1,13 @@
 import { useState  } from "react"
 import { timestamp } from "../../firebase/config"
 import { useAuthContext } from '../../hooks/useAuthContext'
+import { useFirestore } from "../../hooks/useFirestore"
+import Avatar from "../../components/Avatar"
 
-export default function ProjectComments() {
+export default function ProjectComments({project}) {
     const [newComment, setNewComment] = useState('')
     const { user } = useAuthContext()
+    const { updateDocument, response } = useFirestore('projects')
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -16,11 +19,38 @@ export default function ProjectComments() {
             createdAt: timestamp.fromDate(new Date()),
             id: Math.random()
         }
+
+        await updateDocument(project.id, {
+            comments: [...project.comments, commentToAdd]
+        })
+        if(!response.error) {
+            setNewComment('')
+        }
     }
 
     return (
         <div className="project-comments">
             <h4>Project Comments</h4>
+
+            <ul>
+                {
+                    project.comments.length > 0 &&
+                    project.comments.map(comment => (
+                        <li key={comment.id}>
+                            <div className="comment-author">
+                                <Avatar src={comment.photoURL} />
+                                <p>{comment.displayName}</p>
+                            </div>
+                            <div className="comment-date">
+                                <p>date here</p>
+                            </div>
+                            <div className="comment-content">
+                                <p>{comment.content}</p>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
 
             <form className="add-comment" onSubmit={handleSubmit}>
                 <label>
